5747ce501bc4d2570857dbea191df0526668b95f
7conan <angelomarchi05@gmail.com>
2025-07-31 21:27:32 -0300

    login

--- src/app/loginModule/components/login/login.ts
+++ src/app/loginModule/components/login/login.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrls: ['./login.scss']
 })
 export class Login {
+  organizationId = '';
+  username = '';
+  password = '';
 
+  login() {
+    console.log('Login info:', {
+      organizationId: this.organizationId,
+      username: this.username,
+      password: this.password
+    });
+  }
 }
